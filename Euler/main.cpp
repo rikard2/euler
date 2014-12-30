b959edeceb3bceca0bb8229b691aa4b62c4e3fb5
Dani <dani@Rikards-MacBook-Air.local>
2014-12-30 13:55:51 +0100

    Initial Commit

---
+++ Euler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler
+//
+//  Created by Dani on 2014-12-30.
+//  Copyright (c) 2014 Rikard Javelind. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
